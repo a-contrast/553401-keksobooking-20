5caa7e7b423b7d485763203ba5ee1fd46b6c65fc
a-contrast <48984992+a-contrast@users.noreply.github.com>
2020-06-08 20:41:44 +0300

    попытка отобразить карточки объекта

--- js/main.js
+++ js/main.js
@@ -30,14 +30,14 @@ var FEATURES = [
 ];
 // описание жилья
 var DESCRIPTION = [
-  'Описание 1',
-  'Описание 2',
-  'Описание 3',
-  'Описание 4',
-  'Описание 5',
-  'Описание 6',
-  'Описание 7',
-  'Описание 8'
+  // 'Описание 1',
+  // 'Описание 2',
+  // 'Описание 3',
+  // 'Описание 4',
+  // 'Описание 5',
+  // 'Описание 6',
+  // 'Описание 7',
+  // 'Описание 8'
 ];
 // фотографии
 var PHOTOS = [
@@ -48,7 +48,12 @@ var PHOTOS = [
 
 var map = document.querySelector('.map'); // находим блок с картой
 var pinsBlock = document.querySelector('.map__pins'); // находим блок в который будем вставлять наши метки
-var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin'); // находим шаблон метки которую будем вставлять
+var beforeCardsBlock = document.querySelector('.map__filters-container'); // ПЕРЕД этим блоком вставим наши карточки
+// находим шаблон метки которую будем вставлять
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+// находим шаблон карточки которую будем вставлять
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+
 
 // делаем блок видимым
 function mapToggle() {
@@ -95,7 +100,6 @@ function getRandomSliceArray(array) {
 function getObject() {
   // случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка, "y": случайное число, координата y метки на карте от 130 до 630.
   var location = {x: getRandomInRange(0, map.offsetWidth), y: getRandomInRange(130, 630)};
-  var time = getRandomFromArray(TIMES);
 
   return {
     author: {
@@ -109,8 +113,8 @@ function getObject() {
       type: getRandomFromArray(TYPES),
       rooms: getRandomFromArray(ROOMS),
       guests: getRandomFromArray(GUESTS),
-      checkin: time,
-      checkout: time,
+      checkin: getRandomFromArray(TIMES),
+      checkout: getRandomFromArray(TIMES),
       features: getRandomSliceArray(FEATURES),
       description: getRandomFromArray(DESCRIPTION),
       photos: getRandomSliceArray(PHOTOS)
@@ -138,7 +142,7 @@ function getObjectsArray(arrayLength) {
 }
 
 /**
- * Обозначает отображение Pin-а
+ * Готовим Pin для рендеринга
  * @param {object} object - объект из которого будут браться данные для отображения Pin-а
  * @return {object}
  */
@@ -172,6 +176,113 @@ function renderPinsToMap(array) {
   pinsBlock.appendChild(fragment);
 }
 
+/**
+ * возвращает карточку с данными для пина для рендеринга на странице
+ * @param {object} object - объект из массива с рандомными данными
+ * @return {object}
+ */
+function renderCard(object) {
+  var card = cardTemplate.cloneNode(true);
+
+  card.querySelector('.popup__title').textContent = object.offer.title;
+  card.querySelector('.popup__text--address').textContent = object.offer.address;
+  card.querySelector('.popup__text--price').textContent = object.offer.price + '₽/ночь';
+  card.querySelector('.popup__type').textContent = popupType();
+  card.querySelector('.popup__text--capacity').textContent = object.offer.rooms + ' комнаты для ' + object.offer.guests + ' гостей';
+  card.querySelector('.popup__text--time').textContent = 'Заезд после ' + object.offer.checkin + ' , выезд до ' + object.offer.checkout;
+  card.querySelector('.popup__description').textContent = object.offer.description;
+  card.querySelector('.popup__avatar').src = object.author.avatar;
+
+  /**
+   * возвращает фотографию с нужным src для рендеринга в карточке
+   * @param {object} arrialObject
+   * @return {object}
+   */
+  function renderPhoto(arrialObject) {
+    var photo = card.querySelector('.popup__photo').cloneNode(true);
+    photo.src = arrialObject;
+
+    return photo;
+  }
+
+  /**
+   * отрисовывает фотографии в карточке
+   * @param {object} array - массив со ссылками на фотографии
+   */
+  function renderPhotoToCard(array) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < array.length; i++) {
+      fragment.appendChild(renderPhoto(array[i]));
+    }
+
+    card.querySelector('.popup__photos').append(fragment);
+    card.querySelector('.popup__photo' + ':first-child').remove();
+  }
+
+  // возвращает тип жилья
+  function popupType() {
+    var type;
+
+    if (object.offer.type === 'flat') {
+      type = 'Квартира';
+    } else {
+      if (object.offer.type === 'bungalo') {
+        type = 'Бунгало';
+      } else {
+        if (object.offer.type === 'house') {
+          type = 'Дом';
+        } else {
+          type = 'Дворец';
+        }
+      }
+    }
+
+    return type;
+  }
+
+  // убирает блоки из разметки (блоки с опциями, которых нет в выбранном жилье)
+  function popupFeatures() {
+    for (var i = 0; i < FEATURES.length; i++) {
+      if (!object.offer.features.includes(FEATURES[i])) {
+        card.querySelector('.popup__feature--' + FEATURES[i]).remove();
+      }
+    }
+  }
+
+  popupFeatures();
+  renderPhotoToCard(PHOTOS);
+
+  return card;
+}
+
+// создает и возвращает блок для отображения карточек
+function makeMapCardsBlock() {
+  var cardsBlock;
+  beforeCardsBlock.insertAdjacentHTML('beforebegin', '<div class="map__cards"></div>');
+  cardsBlock = document.querySelector('.map__cards');
+
+  return cardsBlock;
+}
+
+/**
+ * отображает карточки на странице
+ * @param {object} array - массив с рандомными объектами
+ */
+function renderCardsToPin(array) {
+  var fragment = document.createDocumentFragment();
+
+  // для начала отрендерим их во fragment
+  for (var i = 0; i < array.length; i++) {
+    fragment.appendChild(renderCard(array[i]));
+  }
+
+  // теперь fragment отобразим на странице
+  cardsBlock.appendChild(fragment);
+}
+
 var mapObjects = getObjectsArray(8); // получаем массив из созданных объектов
+var cardsBlock = makeMapCardsBlock(); // создаем блок для отображения карточек
 mapToggle(); // делаем блок .map видимым
 renderPinsToMap(mapObjects); // отрисовываем пины
+renderCardsToPin(mapObjects); // отрисовываем карточки
