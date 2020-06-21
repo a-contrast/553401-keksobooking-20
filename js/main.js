b06829d0629046b2d6dadd9fa34bba3ae125bd24
a-contrast <48984992+a-contrast@users.noreply.github.com>
2020-06-21 09:01:41 +0300

    разбил функцию по созданию карточки жилья

--- js/main.js
+++ js/main.js
@@ -230,6 +230,73 @@ function unRenderPinsToMap(array) {
 }
 
 /**
+ * возвращает тип жилья
+ * @param {object} object - объект жилья из массива (mapObjects)
+ * @return {string}
+ */
+function popupType(object) {
+  var type;
+  switch (object.offer.type) {
+    case 'flat':
+      type = 'Квартира';
+      break;
+    case 'bungalo':
+      type = 'Бунгало';
+      break;
+    case 'house':
+      type = 'Дом';
+      break;
+    case 'palace':
+      type = 'Дворец';
+      break;
+  }
+
+  return type;
+}
+
+/**
+ * убирает блоки из разметки (блоки с опциями, которых нет в выбранном жилье)
+ * @param {object} object - объект жилья из массива (mapObjects)
+ * @param {object} template - темплейт в котором производим действия
+ */
+function popupFeatures(object, template) {
+  for (var i = 0; i < FEATURES.length; i++) {
+    if (!object.offer.features.includes(FEATURES[i])) {
+      template.querySelector('.popup__feature--' + FEATURES[i]).remove();
+    }
+  }
+}
+
+/**
+ * возвращает фотографию с нужным src для рендеринга в карточке
+ * @param {object} arrialObject - объект (ссылка на фотографию) из массива фотографий
+ * @param {object} template - темплейт в котором производим действия
+ * @return {object}
+ */
+function renderPhoto(arrialObject, template) {
+  var photo = template.querySelector('.popup__photo').cloneNode(true);
+  photo.src = arrialObject;
+
+  return photo;
+}
+
+/**
+ * отрисовывает фотографии в карточке
+ * @param {object} array - массив со ссылками на фотографии
+ * @param {object} template - темплейт в котором производим действия
+ */
+function renderPhotoToCard(array, template) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < array.length; i++) {
+    fragment.appendChild(renderPhoto(array[i], template));
+  }
+
+  template.querySelector('.popup__photos').append(fragment);
+  template.querySelector('.popup__photo' + ':first-child').remove();
+}
+
+/**
  * возвращает карточку с данными для пина для рендеринга на странице
  * @param {object} object - объект из массива с рандомными данными
  * @return {object}
@@ -240,71 +307,14 @@ function renderCard(object) {
   card.querySelector('.popup__title').textContent = object.offer.title;
   card.querySelector('.popup__text--address').textContent = object.offer.address;
   card.querySelector('.popup__text--price').textContent = object.offer.price + '₽/ночь';
-  card.querySelector('.popup__type').textContent = popupType();
+  card.querySelector('.popup__type').textContent = popupType(object);
   card.querySelector('.popup__text--capacity').textContent = object.offer.rooms + ' комнаты для ' + object.offer.guests + ' гостей';
   card.querySelector('.popup__text--time').textContent = 'Заезд после ' + object.offer.checkin + ' , выезд до ' + object.offer.checkout;
   card.querySelector('.popup__description').textContent = object.offer.description;
   card.querySelector('.popup__avatar').src = object.author.avatar;
 
-  /**
-   * возвращает фотографию с нужным src для рендеринга в карточке
-   * @param {object} arrialObject
-   * @return {object}
-   */
-  function renderPhoto(arrialObject) {
-    var photo = card.querySelector('.popup__photo').cloneNode(true);
-    photo.src = arrialObject;
-
-    return photo;
-  }
-
-  /**
-   * отрисовывает фотографии в карточке
-   * @param {object} array - массив со ссылками на фотографии
-   */
-  function renderPhotoToCard(array) {
-    var fragment = document.createDocumentFragment();
-
-    for (var i = 0; i < array.length; i++) {
-      fragment.appendChild(renderPhoto(array[i]));
-    }
-
-    card.querySelector('.popup__photos').append(fragment);
-    card.querySelector('.popup__photo' + ':first-child').remove();
-  }
-
-  // возвращает тип жилья
-  function popupType() {
-    var type;
-    switch (object.offer.type) {
-      case 'flat':
-        type = 'Квартира';
-        break;
-      case 'bungalo':
-        type = 'Бунгало';
-        break;
-      case 'house':
-        type = 'Дом';
-        break;
-      case 'palace':
-        type = 'Дворец';
-        break;
-    }
-
-    return type;
-  }
-
-  // убирает блоки из разметки (блоки с опциями, которых нет в выбранном жилье)
-  function popupFeatures() {
-    for (var i = 0; i < FEATURES.length; i++) {
-      if (!object.offer.features.includes(FEATURES[i])) {
-        card.querySelector('.popup__feature--' + FEATURES[i]).remove();
-      }
-    }
-  }
-
-  popupFeatures();
-  renderPhotoToCard(PHOTOS);
+  popupFeatures(object, card);
+  renderPhotoToCard(PHOTOS, card);
 
   return card;
 }
